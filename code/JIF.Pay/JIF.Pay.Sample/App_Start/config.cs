54e34a950f0865b5deccb21692fa32f1f3b50a1b
且歌且码 <cdmin207078@foxmail.com>
2017-10-23 23:54:14 +0800

    study alipay

---
+++ code/JIF.Pay/JIF.Pay.Sample/App_Start/config.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JIF.Pay.Sample.App_Start
+{
+    public class config
+    {
+        public config()
+        {
+            //
+            // TODO: 在此处添加构造函数逻辑
+            //
+
+        }
+        // 应用ID,您的APPID
+        public static string app_id = "2016081600259106";
+
+        // 支付宝网关
+        //public static string gatewayUrl = "https://openapi.alipay.com/gateway.do";
+
+        public static string gatewayUrl = "https://openapi.alipaydev.com/gateway.do";
+
+        // 商户私钥，您的原始格式RSA私钥
+        public static string private_key = "MIIEpAIBAAKCAQEA4g4Rv5fGxM8Hrs+Ynlxpow44qtGuzuCKUmc8lnVGo1vsOnPzdJmq64UGm8YUGu2TGuwLbBjIX3zBMEy5PODqVWFioNySfu5sCqaODSu7Nhp0/VnoHKM5WLyQ2bgVq9/xbbcuN6hXO3NKZwoT+xiOFbnaInNRKa3AlTcjbuGMRRVsSkqyJgWMReXalHXQqQCDZ8rYU6Ow38W9qB064t0e3qytmgltpdxa1fMp+1j8oVqGQxuu7g0kyXaMfs0I2J8bqSEjkkGfAZK2GjjL4iq5pHjSYLPaUHesjimNbEVaeJJNSVlQXsPcKiHNZJ7PYwKFt+FqCOVs268ce72toL2bGQIDAQABAoIBAGpQP5yh3wOUcoKOc7KWt7/N4mzD7Liy9ZrHnsEMKsqNYs4i1i7SToYxq+f7Al2fuf2BYeXWyfXOkHUMwFmXVd4NzPrmgadcrkdcCZa8KHHLg5XbgMRnJ4NKO72JS/fnfH4jFoNqkowNLsLDBJ6k773cCHEHVvaJFveWVP3C6dta05trwY8DWERy+137WKM8bxx9VVyWAEzq+TAZHkDk8t/yMKWOEnX16IjtCc4DwvcjpTL+VF+rNVO8S+pYsK0NNwZkIjdfn/vp8gvSGs6nDZwXsG4jpXKgARG7uTqlBJagD1DCJGJbJ307a3UWvFQ9hCXhIHmNXOMlSBKnHANpZbkCgYEA9iB/cnTxknvZnEFz4PcszpeZZPIrAIKjP+5krMyyn/z3RD7k7UYQKm4Pn+GXXe38VpB7E3nSTEGzW0QLbNU6uoDXuTfnj6kMgIxcXXT43SyLzJ9Uy1G8J82bvUsK4y2fell6cuExjeTYw9ymBwkhV4a2UGnuhCufoRCcTO/hSHcCgYEA6x9zRB7fY8F7hobHm691kOQ5E521x1i0mucq313Z7oUsdhljZHuzrtiAdlt93rsFT9ozLQVNOftyuqJ0+NcsVOa+p4mHoXKnnYLL6g9KgrC5+JE2wzHIwq6bYGoRW97Am50mVcU0RO6ITgQkKP0fSaOpMfOAdxgQUbUf9c7drO8CgYEAxyBXvCQ85YRp9G9H7zRK/gHMyszaWZlXtnuK2/rocWPaMU6pLAZJQeq1nWaIbigCxY4y5PjkWWm2UBpnG/APiaB/54wBPktIoB/vAnWGenp1yGEH208PaSyB2c4CyvW4VuZZmOP0kZYagavcb0jFvuhwS/LIVApvJYIo0l4o59UCgYEA2As3A+IGTAhN86fGpCRON8utMnwYusqkcS12dHYtOQD/gKkFbiBIU6G2MIsTFmTslZoeqgCUHHIS0rFSIBC/OGgpyZISoWPT5mR38GMKiRnQWDk+g48sNM7yEF6GAs+kVZjJz8f7plczwdQGUe98H1MVtcNUSJpA7IMskdX6VNsCgYBKij5IMI12uIqgRmo/RCc1yyzh6Ey8l2DsDEw8Ew7wcID/ZgAiCqHkerf5vPMMBpJ+3h8R3azqFBPJsX84/M2SlpCkvSapiDPeNs6k9FdTkBtn+1SVhkvsSldtL79cRLSRnqFI0n+H6PRb4kGlXvk95ue8fp3knfVucJ99gwZHjg==";
+
+        // 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+        public static string alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApMuVyYUeQNrRRnUlGLeCzlg8Bje2zXzQG6RD9Srrnwpnqc/zANrcrUKX3KA/u5XJB4Tzykd9gNz+8FyVta6zuDig0GwnXD+m71MpEBBY2BP12iWo9U4NePe/N5MjLArlgWaHUUjLFIovK/BSevxhd/6zSgcwjXdDIpalT62BvzB+9MfWAL277+9132wfTa6piRvWPVykLMrAkwSNuP8ACelFRe9RhdGYtPHIy5JeJMMzSFghwL7WPmLOHIJgK/bxRbXX0qN4GUW3C9SEUdrlN+LyP1kd7V3aa2k0jIZkpjEJj3ACYhC+Z7SKcK6OXolRCAiChXYgdZUYLL0e/p9khQIDAQAB";
+
+        // 签名方式
+        public static string sign_type = "RSA2";
+
+        // 编码格式
+        public static string charset = "UTF-8";
+    }
+}
--- code/JIF.Pay/JIF.Pay.Sample/Controllers/HomeController.cs
+++ code/JIF.Pay/JIF.Pay.Sample/Controllers/HomeController.cs
@@ -1,6 +1,14 @@
-﻿using JIF.Pay.Sample.Models;
+﻿using Aop.Api;
+using Aop.Api.Domain;
+using Aop.Api.Request;
+using Aop.Api.Response;
+using Aop.Api.Util;
+using JIF.Pay.Sample.App_Start;
+using JIF.Pay.Sample.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,7 +28,110 @@ namespace JIF.Pay.Sample.Controllers
         [HttpPost]
         public JsonResult Alipay(CreateOrderInputViewModel order)
         {
-            return AjaxOk("支付宝成功支付");
+            DefaultAopClient client = new DefaultAopClient(config.gatewayUrl, config.app_id, config.private_key, "json", "1.0", config.sign_type, config.alipay_public_key, config.charset, false);
+
+            // 组装业务参数model
+            AlipayTradeWapPayModel model = new AlipayTradeWapPayModel();
+            model.Body = order.Remark.Trim();                                      // 商品描述
+            model.Subject = order.Subject.Trim();                                // 订单名称
+            model.TotalAmount = order.Amount.ToString("0.##");                        // 付款金额
+            model.OutTradeNo = order.OrderNo;                        // 外部订单号，商户网站订单系统中唯一的订单号
+            model.ProductCode = "QUICK_WAP_PAY";
+            //model.QuitUrl = quit_url;                               // 支付中途退出返回商户网站地址
+
+            AlipayTradeWapPayRequest request = new AlipayTradeWapPayRequest();
+            // 设置支付完成同步回调地址
+            request.SetReturnUrl("");
+            // 设置支付完成异步通知接收地址
+            request.SetNotifyUrl("http://192.168.0.107:30001/home/AlipayCallback");
+            // 将业务model载入到request
+            request.SetBizModel(model);
+
+            AlipayTradeWapPayResponse response = null;
+            try
+            {
+                response = client.pageExecute(request, null, "post");
+                return AjaxOk("支付宝 - 成功", response.Body);
+            }
+            catch (Exception exp)
+            {
+                return AjaxFail("支付宝 - 失败", JsonConvert.SerializeObject(exp));
+            }
+        }
+
+
+        public JsonResult AlipayQuery(string orderNo, string aliBusinessOrderNo)
+        {
+            DefaultAopClient client = new DefaultAopClient(config.gatewayUrl, config.app_id, config.private_key, "json", "1.0", config.sign_type, config.alipay_public_key, config.charset, false);
+
+            AlipayTradeQueryModel model = new AlipayTradeQueryModel();
+            model.OutTradeNo = orderNo;                         // 商户订单号，和交易号不能同时为空
+            model.TradeNo = aliBusinessOrderNo;                 // 支付宝交易号，和商户订单号不能同时为空
+
+            AlipayTradeQueryRequest request = new AlipayTradeQueryRequest();
+            request.SetBizModel(model);
+
+            AlipayTradeQueryResponse response = null;
+            try
+            {
+                response = client.Execute(request);
+                return AjaxOk(response.Body);
+            }
+            catch (Exception exp)
+            {
+                return AjaxFail(JsonConvert.SerializeObject(exp));
+            }
+        }
+
+
+        public JsonResult AlipayCallback()
+        {
+            /* 实际验证过程建议商户添加以下校验。
+              1、商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号，
+              2、判断total_amount是否确实为该订单的实际金额（即商户订单创建时的金额），
+              3、校验通知中的seller_id（或者seller_email) 是否为out_trade_no这笔单据的对应的操作方（有的时候，一个商户可能有多个seller_id/seller_email）
+              4、验证app_id是否为该商户本身。
+              */
+            Dictionary<string, string> sArray = GetRequestPost();
+            if (sArray.Count != 0)
+            {
+                bool flag = AlipaySignature.RSACheckV1(sArray, config.alipay_public_key, config.charset, config.sign_type, false);
+                if (flag)
+                {
+                    //交易状态
+                    //判断该笔订单是否在商户网站中已经做过处理
+                    //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                    //请务必判断请求时的total_amount与通知时获取的total_fee为一致的
+                    //如果有做过处理，不执行商户的业务程序
+
+                    //注意：
+                    //退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
+                    string trade_status = Request.Form["trade_status"];
+
+                    return AjaxOk("支付宝 - 回调成功");
+                }
+                else
+                {
+                    return AjaxFail("支付宝 - 回调失败");
+                }
+            }
+
+            return AjaxFail("无信息");
+        }
+
+        public Dictionary<string, string> GetRequestPost()
+        {
+            int i = 0;
+            Dictionary<string, string> sArray = new Dictionary<string, string>();
+            NameValueCollection coll;
+            //coll = Request.Form;
+            coll = Request.Form;
+            String[] requestItem = coll.AllKeys;
+            for (i = 0; i < requestItem.Length; i++)
+            {
+                sArray.Add(requestItem[i], Request.Form[requestItem[i]]);
+            }
+            return sArray;
         }
 
         #endregion 
